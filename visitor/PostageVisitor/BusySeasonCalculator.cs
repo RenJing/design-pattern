71eaff5ab49ae7f1dfb9f70e78bb6ead7fc88f5c
Zhang Yue <zhangyue02135@yahoo.com.cn>
2012-12-13 10:28:52 +0800

    Adjust the const variables for Postage increase rate.

--- visitor/PostageVisitor/BusySeasonCalculator.cs
+++ visitor/PostageVisitor/BusySeasonCalculator.cs
@@ -4,9 +4,9 @@ namespace Visitor.PostageVisitor
 {
     public class BusySeasonCalculator : IPostageCalculator
     {
-        private const double BOOK_POSTAGE_INCREASE_RATE = 0.15;
-        private const double FOOD_POSTAGE_INCREASE_RATE = 0.2;
-        private const double OVEN_POSTAGE_INCREASE_RATE = 0.5;
+        private const double BOOK_POSTAGE_INCREASE_RATE = 1.15;
+        private const double FOOD_POSTAGE_INCREASE_RATE = 1.2;
+        private const double OVEN_POSTAGE_INCREASE_RATE = 1.5;
         private readonly IPostageCalculator postageCalculator;
 
         public BusySeasonCalculator(IPostageCalculator postageCalculator)
@@ -16,17 +16,17 @@ namespace Visitor.PostageVisitor
 
         public double Visit(Book book)
         {
-            return postageCalculator.Visit(book)*(1 + BOOK_POSTAGE_INCREASE_RATE);
+            return postageCalculator.Visit(book) * BOOK_POSTAGE_INCREASE_RATE;
         }
 
         public double Visit(Oven oven)
         {
-            return postageCalculator.Visit(oven)*(1 + OVEN_POSTAGE_INCREASE_RATE);
+            return postageCalculator.Visit(oven) * OVEN_POSTAGE_INCREASE_RATE;
         }
 
         public double Visit(Food food)
         {
-            return postageCalculator.Visit(food)*(1 + FOOD_POSTAGE_INCREASE_RATE);
+            return postageCalculator.Visit(food) * FOOD_POSTAGE_INCREASE_RATE;
         }
     }
 }
